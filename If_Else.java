6d4c5f581c7abe96d3b6081ec845d4b7b6a43152
JaminSun <b.tinson@hotmail.com>
2019-06-03 11:40:27 +0100

    Day 1

---
+++ If_Else.java
@@ -0,0 +1,19 @@
+class Results{
+    public static void main(String xyz[]) {
+        int phy, che, mat, total;
+        float per;
+        phy=50;
+        che=50;
+        mat=100;
+        total=phy+che+mat;
+        per=total/100*450;
+        System.out.println(total);
+        System.out.println("Percentage: ");
+        if(per>=60){
+            System.out.println("You have passed");
+        }
+        if(per<60){
+            System.out.println("You have failed");
+        }
+    }
+}
